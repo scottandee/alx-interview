01d8b8d3bfce466b2a5a44bd023072bb7ff28dde
Andee Vagrant Vscode <andeeolayinkascott@gmail.com>
2023-12-09 16:14:19 +0000

    Solution to lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This script contains a function that
+determines if a function can be opened"""
+
+
+def open_box(box, all_boxes, opened_boxes):
+    if len(box) == 0:
+
+        return opened_boxes
+    for key in box:
+        if key not in opened_boxes:
+            opened_boxes.append(key)
+            opened_boxes = open_box(all_boxes[key], all_boxes, opened_boxes)
+        else:
+            continue
+    return opened_boxes
+
+
+def canUnlockAll(boxes):
+    """This function takes an array of array(boxes) of
+    integers(keys) as parameter and determines if all
+    the boxes can be opened
+    """
+    opened_boxes = [0]
+    opened_boxes = open_box(boxes[0], boxes, opened_boxes)
+
+    if len(boxes) != len(opened_boxes):
+        return False
+    else:
+        return True
