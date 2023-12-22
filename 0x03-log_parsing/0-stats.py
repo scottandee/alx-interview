59df84a50763927801595b82ea6879d088959be0
Andee WSL <andeeolayinkascott@gmail.com>
2023-12-22 03:40:00 +0100

    Pycodestyle fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,7 +14,8 @@ try:
     for line in sys.stdin:
         num_of_read += 1
         r = re.search(
-            '^\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}\\s-\\s\\[[\\d -:.]*\\]\\s"GET\\s\\/projects\\/260\\sHTTP\\/1.1"\\s\\d{1,3}\\s\\d{1,4}$',
+            '^\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}\\s-\\s\\[[\\d -:.]*\
+\\]\\s"GET\\s\\/projects\\/260\\sHTTP\\/1.1"\\s\\d{1,3}\\s\\d{1,4}$',
             line)
         if r:
             status = re.search("(?<=1.1\" )\\d{1,3}", line)
@@ -34,3 +35,4 @@ except KeyboardInterrupt:
     print(f"File size: {total_file_size}")
     for status in sorted(status_code_count):
         print(f"{status}: {status_code_count[status]}")
+    raise
