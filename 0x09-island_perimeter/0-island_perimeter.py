f5c13a7b641418b916b2cd3ae629042c644caeca
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-03-07 19:43:15 +0100

    Refactor code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,41 +10,46 @@ def island_perimeter(grid):
     of the island found in the 2D array grid
     passed as parameter
     """
-    p = 0
+    perimeter = 0
     rows = len(grid)
     cols = len(grid[0])
 
     # Iterate through all elements of the grid
     for r in range(rows):
         for c in range(cols):
-            if grid[r][c] == 0:
-                continue
+            if grid[r][c] == 1:
+                perimeter += 4
 
-            # check the contents of the top
-            if r - 1 < 0:
-                p += 1
-                continue
-            if grid[r - 1][c] == 0:
-                p += 1
+                if r > 0 and grid[r - 1][c] == 1:
+                    perimeter -= 2
+                if c > 0 and grid[r][c - 1] == 1:
+                    perimeter -= 2
 
-            # check the contents of the bottom
-            if r + 1 > rows - 1:
-                p += 1
-                continue
-            if grid[r + 1][c] == 0:
-                p += 1
+            # # check the contents of the top
+            # if r - 1 < 0 and :
+            #     p += 1
+            #     continue
+            # if grid[r - 1][c] == 0:
+            #     p += 1
 
-            # check the contents of the left side
-            if c - 1 < 0:
-                p += 1
-                continue
-            if grid[r][c - 1] == 0:
-                p += 1
+            # # check the contents of the bottom
+            # if r + 1 > rows - 1:
+            #     p += 1
+            #     continue
+            # if grid[r + 1][c] == 0:
+            #     p += 1
 
-            # check the contents of the right side
-            if c + 1 > cols - 1:
-                p += 1
-                continue
-            if grid[r][c + 1] == 0:
-                p += 1
-    return p
+            # # check the contents of the left side
+            # if c - 1 < 0:
+            #     p += 1
+            #     continue
+            # if grid[r][c - 1] == 0:
+            #     p += 1
+
+            # # check the contents of the right side
+            # if c + 1 > cols - 1:
+            #     p += 1
+            #     continue
+            # if grid[r][c + 1] == 0:
+            #     p += 1
+    return perimeter
