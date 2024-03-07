1c74d7609823ee2a6954ff9d2fc88e87fb2701bb
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-03-07 20:02:47 +0100

    Clean up

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -18,38 +18,12 @@ def island_perimeter(grid):
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
+                # Assume each grid has a perimeter of 4
                 perimeter += 4
+                # Check if adjacent sides are land
 
                 if r > 0 and grid[r - 1][c] == 1:
                     perimeter -= 2
                 if c > 0 and grid[r][c - 1] == 1:
                     perimeter -= 2
-
-            # # check the contents of the top
-            # if r - 1 < 0 and :
-            #     p += 1
-            #     continue
-            # if grid[r - 1][c] == 0:
-            #     p += 1
-
-            # # check the contents of the bottom
-            # if r + 1 > rows - 1:
-            #     p += 1
-            #     continue
-            # if grid[r + 1][c] == 0:
-            #     p += 1
-
-            # # check the contents of the left side
-            # if c - 1 < 0:
-            #     p += 1
-            #     continue
-            # if grid[r][c - 1] == 0:
-            #     p += 1
-
-            # # check the contents of the right side
-            # if c + 1 > cols - 1:
-            #     p += 1
-            #     continue
-            # if grid[r][c + 1] == 0:
-            #     p += 1
     return perimeter
